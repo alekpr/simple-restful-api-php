4dce73e6298bcb777197aeb4a4361448b35f024b
Direk Prama <alek.pr@gmail.com>
2015-07-19 01:55:17 +0700

    remove my password in const.php

--- libs/const/const.php
+++ libs/const/const.php
@@ -4,10 +4,10 @@
 define('WEBSERVICE_URL', 'http://myrestful.local'); // webservice url
 
 define('DB_TYPE', 'mysqli'); // database connection type
-define('DB_HOST', 'localhost'); // database host
-define('DB_USER', 'root'); // database username
-define('DB_PASS', '11939774'); // database password
-define('DB_NAME', 'test'); // database name
+define('DB_HOST', 'YOUR_DB_HOST'); // database host
+define('DB_USER', 'YOUR_DB_USERNAME'); // database username
+define('DB_PASS', 'YOUR_DB_PASSWORD'); // database password
+define('DB_NAME', 'YOUR_DB_NAME'); // database name
 define('TB_COFFEE', 'coffee'); // table name
 
 
